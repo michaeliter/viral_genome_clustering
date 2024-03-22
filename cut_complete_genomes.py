e09210bd9d066e2977533f3ce850ae39fe922121
michaeliter <42823460+michaeliter@users.noreply.github.com>
2024-03-21 21:38:09 -0700

    Add files via upload

---
+++ cut_complete_genomes.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from Bio import SeqIO
+
+seq_ids = pd.read_csv("complete_seqs.txt", header=None)
+proviruses = list(SeqIO.parse("proviruses.fna", "fasta"))
+lytic_viruses = list(SeqIO.parse("viruses.fna", "fasta"))
+
+complete_data = []
+for seq in seq_ids[0]:
+  check1 = seq + "_"
+  check2 = seq + " "
+  count = 0
+  for record in proviruses:
+    if check1 in record.id or check2 in record.id:
+      count += 1
+      complete_data.append(record)
+  if count > 0:
+    print(count)
+  else:
+    for record in lytic_viruses:
+      if seq == record.id:
+        complete_data.append(record)
+        count += 1
+    if count == 0:
+      print(f"Something went wrong with contig {seq}")
+      
+SeqIO.write(complete_data, "pal_complete_cut.fa", "fasta")
